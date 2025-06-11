afa74b7aaeb0e17c131848e293f95199cb273e07
caverace <135933781+caverace@users.noreply.github.com>
2025-06-11 10:05:36 +1000

    Initial shadcn package add
    + shadcn and button

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import "./App.css";
+import "./globals.css";
+import { Button } from "@/components/ui/button";
 
 function App() {
     const [count, setCount] = useState(0);
@@ -7,12 +8,7 @@ function App() {
     return (
         <div className="w-full flex flex-col gap-2 justify-center items-center h-[100vh]">
             <h1 className="text-3xl font-bold">Hello world 🦈</h1>
-            <button
-                className="border p-2 rounded shadow hover:bg-gray-100 transition-all"
-                onClick={() => setCount(count + 1)}
-            >
-                Count: {count}
-            </button>
+            <Button onClick={() => setCount(count + 1)}>Count: {count}</Button>
         </div>
     );
 }

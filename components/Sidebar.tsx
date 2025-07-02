107804b06683d1f7320c7628468d544a44f0b735
caverace <jack.barlow812@gmail.com>
2025-07-02 23:50:23 +1000

    Crude sidebar opening

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,18 +1,20 @@
-import * as React from "react";
+import { useState, type MouseEventHandler, type ReactNode } from "react";
 import { Button } from "@/components/ui/button";
 import { BrickWall, PanelLeftOpen, Ruler } from "lucide-react";
 
-function SideButton({ children }: { children: React.ReactNode }) {
+function SideButton({ children, onClick }: { children: ReactNode, onClick?: MouseEventHandler<HTMLButtonElement> }) {
     return (
-        <Button variant="outline" className="size-12">
+        <Button variant="outline" className="size-12 w-full" onClick={onClick}>
             {children}
         </Button>
     );
 }
 
 function SideBar() {
+    const [ isOpen, setOpen ] = useState<boolean>(false);
+
     return (
-        <div className="border-r p-2 flex flex-col gap-2 justify-between [&_svg:not([class*='size-'])]:size-8">
+        <div className={`${isOpen ? "w-64" : null} border-r p-2 flex flex-col gap-2 justify-between [&_svg:not([class*='size-'])]:size-6`}>
             <div className="flex flex-col gap-2">
                 <SideButton>
                     <Ruler />
@@ -21,7 +23,7 @@ function SideBar() {
                     <BrickWall />
                 </SideButton>
             </div>
-            <SideButton>
+            <SideButton onClick={() => setOpen(!isOpen)}>
                 <PanelLeftOpen />
             </SideButton>
         </div>
